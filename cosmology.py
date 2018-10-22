adf39120c646808fd0e6a29578e74711abb88e2c
[Boyuan Liu] <[treibeis1995@gmail.com]>
2018-10-21 21:17:44 -0500

    update Tb

--- cosmology.py
+++ cosmology.py
@@ -8,18 +8,23 @@ GRA = 6.672e-8
 BOL = 1.3806e-16
 #BOL = 1.0
 PROTON = 1.6726e-24
-#PROTON = 1.6726e-24/(2.176e-5)
 ELECTRON = 9.10938356e-28
 HBAR = 1.05457266e-27
+PLANCK = HBAR*2*np.pi
 CHARGE = 4.80320451e-10
-H_FRAC = 0.76
 SPEEDOFLIGHT = 2.99792458e+10
-
+eV = 1.60218e-12
+YR = 3600*24*365
+PC = 3.085678e18
 SIGMATH = 8*np.pi*(CHARGE**2/ELECTRON/SPEEDOFLIGHT**2)**2/3
 
+# Primordial abundances
 Hfrac = 0.76
 XeH = 0.0
 XeHe = 0.0
+def mmw(xeH = XeH, xeHe = XeHe, X = Hfrac):
+	xh = 4*X/(1+3*X)
+	return 4.0/(1.0+3*X)/(xh*(1+xeH)+(1-xh)*(1+xeHe))
 
 # Units
 UL = 3.085678e21
@@ -36,7 +41,6 @@ UE = UM*UL**2/UT**2
 
 G = GRA*UM*UT**2/UL**3
 
-YR = 3600*24*365
 # Cosmology
 def H(a, Om = 0.315, h = 0.6774, OR = 9.54e-5):
 	H0 = h*100*UV/UL/1e3
@@ -53,8 +57,6 @@ def dt_da(a, Om = 0.315, h = 0.6774, OR = 9.54e-5):
 	return 1/a/H(a, Om, h, OR)
 
 def TZ(z, Om = 0.315, h = 0.6774, OR = 9.54e-5):
-	#def integrand(a):
-	#	return 1/a/H(a, Om, h)
 	I = quad(dt_da, 0, 1/(1+z), args = (Om, h, OR), epsrel = 1e-8)
 	return I[0]
 
@@ -66,18 +68,28 @@ ld0 = [DZ(x)/UL/1e3 for x in lz0]
 ZT = interp1d(lt0, lz0)
 ZD = interp1d(ld0, lz0)
 
-def T_cosmic(z, alpha = -4, beta = 1.27, z0 = 189.6, zi = 1100, T0 = 3300):
-	def integrand(logt):
-		return alpha/3.0-(2+alpha)/3.0*(1-np.exp(-(ZT(logt)/z0)**beta))
-	I = quad(integrand, np.log10(TZ(zi)/1e9/YR), np.log10(TZ(z)/1e9/YR))[0]#, epsrel = 1e-6)[0]
-	temp = T0*10**I
-	return temp
-
 def rhom(a, Om = 0.315, h = 0.6774):
 	H0 = h*100*UV/UL/1e3
 	rho0 = Om*H0**2*3/8/np.pi/GRA
 	return rho0/a**3
 
+def T_cosmic(z, alpha = -4, beta = 1.27, z0 = 189.6, zi = 1020, T0 = 2.726*1021):
+	def integrand(logt):
+		return alpha/3.0-(2+alpha)/3.0*(1-np.exp(-(ZT(logt)/z0)**beta))
+	I = quad(integrand, np.log10(TZ(zi)/1e9/YR), np.log10(TZ(z)/1e9/YR))[0], epsrel = 1e-6)[0]
+	temp = T0*10**I
+	return temp
+
+def T_b(z, a1=1./119, a2=1./115, T0=2.726):
+	a = 1./(1+z)
+	return T0/(a*(1+a/(a1*(1+(a2/a)**1.5))))
+
+def T_dm(z, m = 1., T0=2.726):
+	zc = m*eV / ((3./2)*BOL*T0) - 1
+	Tc = T0*(1+zc)
+	return T0*(1+z) * (z>zc) + Tc*((1+z)/(1+zc))**2 * (z<=zc)
+
+# DM haloes
 def tff(z = 10.0, delta = 200):
 	return (3*np.pi/(32*GRA*delta*rhom(1/(1+z))))**0.5
 
@@ -85,10 +97,6 @@ def Lvir(m = 1e10, z = 10.0, delta = 200):
 	M = m*UM/1e10
 	Rvir = (M/(rhom(1/(1+z))*delta)*3/4/np.pi)**(1/3)
 	return 3*GRA*M**2/Rvir/tff(z, delta)/5
-
-def mmw(xeH = XeH, xeHe = XeHe, X = Hfrac):
-	xh = 4*X/(1+3*X)
-	return 4.0/(1.0+3*X)/(xh*(1+xeH)+(1-xh)*(1+xeHe))
 
 def Tvir(m = 1e10, z = 10.0, delta = 200):
 	M = m*UM/1e10
