42c1bcff04dfe95e64c74c65bf4940060c1296c0
Treibeis <treibeis1995@gmail.com>
2019-08-05 22:02:58 -0500

    nautilus

---
+++ forfun/nautilus.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+phi = (1+5**0.5)/2
+
+def P2C(r, theta):
+	x = r*np.cos(theta)
+	y = r*np.sin(theta)
+	return x, y
+	
+def s2r(s, a = 1, c = phi**(2/np.pi)):
+	return a*c**s
+	
+npoint = 1000
+ncircle = 3.25
+ls = np.linspace(0, ncircle*2*np.pi, npoint)
+lr = s2r(ls)
+
+lx, ly = P2C(lr, ls)
+
+c = 'r'
+plt.figure()
+ax = plt.subplot(111)
+ax.set_aspect(aspect=1)
+plt.plot(lx, ly, color=c)
+plt.title(r'Golden spirial')#: $N_{c}='+str(ncircle)+'$')
+plt.tight_layout()
+plt.savefig('nautilus.pdf')
+plt.show()
+plt.close()
